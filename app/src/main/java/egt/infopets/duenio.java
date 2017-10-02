b871013f3c7f240a11815be278c8642ca0ffc663
Sebastian Salinas <s.salinassalinas@gmail.com>
2017-10-02 12:32:55 -0300

    vista searchPet

---
+++ app/src/main/java/egt/infopets/duenio.java
@@ -0,0 +1,37 @@
+package egt.infopets;
+
+/**
+ * Created by Soporte on 02-10-2017.
+ */
+
+public class duenio {
+
+    private String nombre;
+    private String numero;
+    private String direccion;
+
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+}
---
+++ app/src/main/java/egt/infopets/mascota.java
@@ -0,0 +1,22 @@
+package egt.infopets;
+
+/**
+ * Created by Soporte on 02-10-2017.
+ */
+
+public class mascota {
+
+    public String id;
+    public String mNombre;
+    public String fNacimiento;
+    public String especie;
+    public String raza;
+    public String sexo;
+    public String color;
+    public String[] diagnostico;
+    public String nombre;
+    public String direccion;
+    public String numero;
+
+
+}
