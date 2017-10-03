7b7ffee5acf5820ceaebfeea611358f29fe957b7
Sebastian Salinas <s.salinassalinas@gmail.com>
2017-10-03 09:01:05 -0300

    se agregan las vistas editPet y MantenedorSpeciesAndRace

---
+++ app/src/main/java/egt/infopets/editPet.java
@@ -0,0 +1,13 @@
+package egt.infopets;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class editPet extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_pet);
+    }
+}
---
+++ app/src/main/java/egt/infopets/mantenedorSpeciesAndRace.java
@@ -0,0 +1,13 @@
+package egt.infopets;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class mantenedorSpeciesAndRace extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mantenedor_species_and_race);
+    }
+}
