90f4ecff87a9a4719ff2832fe4d9cfac1a8aa5a0
Sebastian Salinas <s.salinassalinas@gmail.com>
2017-12-20 11:21:32 -0300

    Se agregan listados en estadisticas Se descarta el dialog informe.xml Se agrega Spinner y carga de Spinner en Estadisticas

--- app/src/main/java/egt/infopets/Vistas/Estadisticas.java
+++ app/src/main/java/egt/infopets/Vistas/Estadisticas.java
@@ -4,27 +4,97 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
 import egt.infopets.Clases.Especie;
 import egt.infopets.Clases.Mascota;
+import egt.infopets.Clases.Raza;
 import egt.infopets.Mantenedores.SQLite.MantenedorEspecie;
 import egt.infopets.Mantenedores.SQLite.MantenedorMascota;
+import egt.infopets.Mantenedores.SQLite.MantenedorRaza;
 import egt.infopets.R;
 
 public class Estadisticas extends AppCompatActivity {
+    String[] listaString;
+    Spinner seleccion;
+    ListView list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_estadisticas);
+
+        seleccion = (Spinner) findViewById(R.id.spSeleccion);
+        list = (ListView) findViewById(R.id.lvInfoListar);
+
+        cargarSpinner();
+
+        ArrayAdapter<CharSequence> adaptador = new ArrayAdapter(this, android.R.layout.simple_spinner_item, listaString);
+        seleccion.setAdapter(adaptador);
+
     }
 
-    private void mostrar() {
-        final MantenedorEspecie mantenedorEspecie =new MantenedorEspecie(this);
+    private void cargarSpinner() {
+
+        listaString = new String[6];
+        listaString[0] = "Seleccion";
+        listaString[1] = "Especie";
+        listaString[2] = "Raza";
+        listaString[3] = "Dueño";
+        listaString[4] = "Visitas";
+        listaString[5] = "Mascotas";
+    }
+
+    private void mostrar(int posicion) {
+        switch (posicion) {
+            case 1:
+                list.setAdapter(null);
+                cargarEspecies();
+                break;
+            case 2:
+                list.setAdapter(null);
+                cargarRazas();
+                break;
+            default:
+                list.setAdapter(null);
+                mensaje("Ha ocurrido un error inesperado");
+        }
+    }
+
+    private void cargarRazas() {
+        final MantenedorRaza mantenedorEspecie = new MantenedorRaza(this);
+
+        final List<Raza> auxListaRaza = mantenedorEspecie.getAll();
+
+        String[] listaString = new String[auxListaRaza.size()];
+
+        Iterator iter = auxListaRaza.iterator();
+
+        int pos = 0;
+
+        while (iter.hasNext()) {
+            Raza auxLista = new Raza();
+
+            auxLista = (Raza) iter.next();
+
+            listaString[pos] = auxLista.getDescripcion();
+            pos++;
+        }
+
+        final ListView auxListView = (ListView) findViewById(R.id.lvInfoListar);
+
+        auxListView.setAdapter(new ArrayAdapter(this, android.R.layout.simple_list_item_1, listaString));
+    }
+
+    private void cargarEspecies() {
+        final MantenedorEspecie mantenedorEspecie = new MantenedorEspecie(this);
 
         final List<Especie> auxListaEspecie = mantenedorEspecie.getAll();
 
@@ -34,7 +104,7 @@ public class Estadisticas extends AppCompatActivity {
 
         int pos = 0;
 
-        while (iter.hasNext()){
+        while (iter.hasNext()) {
             Especie auxLista = new Especie();
 
             auxLista = (Especie) iter.next();
@@ -43,13 +113,22 @@ public class Estadisticas extends AppCompatActivity {
             pos++;
         }
 
-        final ListView auxListView = (ListView)findViewById(R.id.lvInfoListar);
+        final ListView auxListView = (ListView) findViewById(R.id.lvInfoListar);
 
-        auxListView.setAdapter(new ArrayAdapter(this,android.R.layout.simple_list_item_1,listaString));
+        auxListView.setAdapter(new ArrayAdapter(this, android.R.layout.simple_list_item_1, listaString));
 
     }
 
     public void buscar(View view) {
-        mostrar();
+        if (seleccion.getSelectedItemPosition() == 0) {
+            list.setAdapter(null);
+            mensaje("Debe seleccionar lo que desea buscar");
+        } else {
+            mostrar(seleccion.getSelectedItemPosition());
+        }
+    }
+
+    public void mensaje(String mensaje) {
+        Toast.makeText(this, mensaje, Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/egt/infopets/Vistas/FirstScreen.java
+++ app/src/main/java/egt/infopets/Vistas/FirstScreen.java
@@ -1,82 +1,28 @@
 package egt.infopets.Vistas;
 
-import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageButton;
-import android.widget.ListView;
-import android.widget.Spinner;
 import android.widget.Toast;
 
-import java.util.Iterator;
-import java.util.List;
-
-import egt.infopets.Clases.Especie;
-import egt.infopets.Clases.Visitas;
-import egt.infopets.Mantenedores.SQLite.MantenedorEspecie;
-import egt.infopets.Mantenedores.SQLite.MantenedorVisitas;
 import egt.infopets.R;
 import egt.infopets.Clases.Mascota;
-import egt.infopets.Mantenedores.SQLite.DbInfoPet;
 import egt.infopets.Mantenedores.SQLite.MantenedorMascota;
 
 public class FirstScreen extends AppCompatActivity {
-
-    private DbInfoPet conector;
-    private Context context;
-    private String tabla;
-    String[] listaStringSeleccion;
-    Spinner spInfoSpinner;String auxVar = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first_screen);
-        final MantenedorEspecie mantenedorEspecie =new MantenedorEspecie(this);
-        final  ImageButton mShowDialog = ( ImageButton) findViewById(R.id.btnInfoEstadistica);
-        mShowDialog.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
 
-                AlertDialog.Builder mBuider = new AlertDialog.Builder(FirstScreen.this);
-                final View mView = getLayoutInflater().inflate(R.layout.informe, null);
-                final EditText mTextBusca = (EditText) mView.findViewById(R.id.txtInfoBuscars);
-                Button mImageButton = (Button) mView.findViewById(R.id.btnInfoBuscars);
-                ListView mLista = (ListView) mView.findViewById(R.id.lvInfoListars);
-
-                mBuider.setView(mView);
-                final AlertDialog dialog = mBuider.create();
-                dialog.show();
-
-                mImageButton.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-
-
-                    }
-                });
-            }
-        });
-    }
-
-    public void mostrar(){
-
-    }
-
-    public void goToInfoEstadisticas(View view){
-        Intent intent = new Intent(this, Estadisticas.class);
-        startActivity(intent);
     }
 
     public void goToEstadisticas(View view){
-
+        Intent intent = new Intent(this, Estadisticas.class);
+        startActivity(intent);
     }
 
     public void goToAddPet(View view) {
